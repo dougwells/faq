33689d194691c6be052e7ac6a8ceadadd5b80848
dougwells <dougwells@gmail.com>
2015-10-27 13:26:24 -0600

    Fixed {{each}} loop bug in questions.html

--- client/templates/questions/questions.js
+++ client/templates/questions/questions.js
@@ -1,1 +1,9 @@
+Template.questions.helpers({
+   questions: function(){
+       return Questions.find({}).map(function(question, index){
+            question.i=index;
+           return question;
+       });
+   }
+});
 
